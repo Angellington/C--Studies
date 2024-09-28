6a5194789afef11a76d63cd614b2497eee1b20f3
Yukenshi7230 <61129075+Angellington7230@users.noreply.github.com>
2024-09-28 14:20:22 -0300

    Input and Output lib + cmd lib

---
+++ variableText.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main() {
+    system("cls");
+    // Definindo Variáveis
+    int idade = 19;
+    float altura = 1.78;
+    char inicial = 'A';
+    bool aprovado = true;
+    std::string nome = "Angel";
+
+    std::cout << "Nome: " << nome << std::endl;
+    std::cout << "Idade: " << idade << std::endl;
+    std::cout << "Altura: " << altura << "m" << std::endl;
+    std::cout << "Inicial: " << inicial << std::endl;
+    std::cout << "Aprovado: " << aprovado << std::endl;
+
+    return 0;
+
+}
