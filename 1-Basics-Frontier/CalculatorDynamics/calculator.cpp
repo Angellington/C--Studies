d3783e605cdb3d38b8492f3dbd6dbe02685d7123
Yukenshi7230 <61129075+Angellington@users.noreply.github.com>
2024-10-19 23:49:12 -0300

    Hello World C++

---
+++ 1-Basics-Frontier/CalculatorDynamics/calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+// Bibliotecas acima
+
+// Evitar o uso std
+using namespace std;
+
+int main(){
+    system("cls");
+    std::setlocale(LC_ALL, "pt_BR.UTF-8");
+    std::setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    cout << "Hello World" << endl;
+
+    return 0;
+}
+
